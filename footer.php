e22948129a710d9a0eca798068924f44d2ade3ad
Benjamin Thomas-Gill <ben@stormbox.com.au>
2023-08-30 21:15:18 +0800

    Tweak
    Footer clean up

--- footer.php
+++ footer.php
@@ -11,62 +11,7 @@
 
 ?>
 	<footer id="colophon" class="site-footer">
-		<!-- <div class="ct-flex footer-container">
-			<div class="ct-20">
-				<ul>
-					<li><strong>Footer Header 1</strong></li>
-					<li>Footer item 1</li>
-					<li>Footer item 2</li>
-					<li>Footer item 3</li>
-				</ul>
-			</div>
-			<div class="ct-20">	
-				<ul>
-					<li><strong>Footer Header 2</strong></li>
-					<li>Footer item 1</li>
-					<li>Footer item 2</li>
-					<li>Footer item 3</li>
-				</ul>
-			</div>
-			<div class="ct-20">	
-				<ul>
-					<li><strong>Footer Header 3</strong></li>
-					<li>Footer item 1</li>
-					<li>Footer item 2</li>
-					<li>Footer item 3</li>
-				</ul>
-			</div>
-			<div class="ct-20">	
-				<ul>
-					<li><strong>Footer Header 4</strong></li>
-					<li>Footer item 1</li>
-					<li>Footer item 2</li>
-					<li>Footer item 3</li>
-				</ul>
-			</div>
-			<div class="ct-20">	
-				<p><?php echo primary_details()['business_name']; ?><br>
-				<?php echo primary_details()['street_address']; ?>,<br>
-				<?php echo primary_details()['suburb']; ?> <?php echo primary_details()['state']; ?> <?php echo primary_details()['postcode']; ?><br>
-				Phone: <?php echo primary_details()['phone']; ?>
-				</p>
-			</div>
-		</div> -->
-
-
-		<!-- <div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'customtheme' ) ); ?>">
-
-			</a>
-			<span class="sep">© Copyright <?php echo date('Y'); ?>  | Custom Theme By <a href="http://benthomasgill.com.au">Ben Thomas-Gill</a></span>
-		</div>.site-info -->
-
-		<!--/ Start Local Business Schema /-->
-		<div itemscope itemtype="https://schema.org/LocalBusiness">
-			<?php get_template_part('inc/footer/business-details'); ?> 
-		</div>
-		<!--/ End Local Business Schema /-->
-
+		<!-- That's one empty footer! -->
 	</footer><!-- #colophon -->
 
 </div><!-- #page -->
